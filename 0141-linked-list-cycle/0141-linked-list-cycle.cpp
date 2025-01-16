b505cd13591ce220486d7bb6cdfffa96b04582c6
ktechbyte <work.khushalsachdeva@gmail.com>
2025-01-16 21:04:28 +0530

    Time: 6 ms (90.76%), Space: 11.8 MB (68.73%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,14 +10,26 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         //hashmap solution (O(n) time and space)
-        unordered_set<ListNode*> mpp;
-        ListNode *temp = head;
-        while(temp){
-            if(mpp.find(temp)!=mpp.end()){
-                return true;
-            }
-            mpp.insert(temp);
-            temp = temp->next;
+        // unordered_set<ListNode*> mpp;
+        // ListNode *temp = head;
+        // while(temp){
+        //     if(mpp.find(temp)!=mpp.end()){
+        //         return true;
+        //     }
+        //     mpp.insert(temp);
+        //     temp = temp->next;
+        // }
+        // return false;
+
+        // optimised solution using slow and fast pointer (O(n) time and O(1) space)
+        if(head==NULL || head->next==NULL) return false;
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while(fast->next && fast->next->next){
+            fast = fast->next->next;
+            slow=slow->next;
+            if(slow==fast) return true;
         }
         return false;
     }
