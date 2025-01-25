036a1688935619345a53c3563e5280acad4ec45e
ktechbyte <work.khushalsachdeva@gmail.com>
2025-01-25 23:13:09 +0530

    Time: 0 ms (100%), Space: 11.5 MB (25.21%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        // using stack (using O(n) extra space)
-        //      stack<string> st;
-        //      int n = s.size();
+        // without using stack
+        // string st;
+        // string ans;
+        // int n = s.size();
 
-        //     for (int i = 0; i < n;) {
-        //         if (s[i] != ' ') {
-        //             int j = i;
-        //             while (j < n && s[j] != ' ') {
-        //                 j++;
-        //             }
-        //             st.push(s.substr(i, j - i));
-        //             i = j;
-        //         } else {
-        //             i++;
+        // for (int i = 0; i < n;) {
+        //     if (s[i] != ' ') {
+        //         int j = i;
+        //         while (j < n && s[j] != ' ') {
+        //             j++;
         //         }
+        //         st = s.substr(i, j - i);
+        //         ans = st + (ans.empty() ? "" : " ") + ans;
+        //         i = j;
+        //     } else {
+        //         i++;
         //     }
-
-        //     string ans;
-        //     while (!st.empty()) {
-        //         ans.append(st.top());
-        //         st.pop();
-        //         if (!st.empty()) {
-        //             ans += ' ';
-        //         }
-        //     }
-
-        //     return ans;
         // }
+        // return ans;
 
-        string st;
-        string ans;
+        // using stack (using O(n) extra space)
+        stack<string> st;
         int n = s.size();
 
         for (int i = 0; i < n;) {
@@ -40,13 +31,22 @@ public:
                 while (j < n && s[j] != ' ') {
                     j++;
                 }
-                st = s.substr(i, j - i);
-                ans = st + (ans.empty() ? "" : " ") + ans;
+                st.push(s.substr(i, j - i));
                 i = j;
             } else {
                 i++;
             }
         }
+
+        string ans;
+        while (!st.empty()) {
+            ans.append(st.top());
+            st.pop();
+            if (!st.empty()) {
+                ans += ' ';
+            }
+        }
+
         return ans;
     }
 };
