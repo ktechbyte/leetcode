bf07fea1f5815ea3a8a942a2929d5cd4f8a987e1
ktechbyte <work.khushalsachdeva@gmail.com>
2025-05-19 23:36:55 +0530

    Time: 0 ms (100%), Space: 23 MB (8.96%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.cpp
+++ 3321-type-of-triangle/3321-type-of-triangle.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     string triangleType(vector<int>& nums) {
-        int s1 = nums[0] + nums[1], s2 = nums[1] + nums[2], s3 = nums[2] + nums[0];
-        if(s1<=nums[2] || s2<=nums[0] || s3<=nums[1]) return "none";
-        if(nums[0] == nums[1] && nums[1] == nums[2] && nums[0] == nums[2]) return "equilateral";
-        else if (nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2]) return "scalene";
-        return "isosceles";
+        // int s1 = nums[0] + nums[1], s2 = nums[1] + nums[2], s3 = nums[2] + nums[0];
+        // if(s1<=nums[2] || s2<=nums[0] || s3<=nums[1]) return "none";
+        // if(nums[0] == nums[1] && nums[1] == nums[2] && nums[0] == nums[2]) return "equilateral";
+        // else if (nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2]) return "scalene";
+        // return "isosceles";
+        sort(nums.begin(), nums.end());
+        if(nums[0] + nums[1] <= nums[2]) return "none";
+        else if(nums[0] == nums[2]) return "equilateral";
+        else if(nums[0] == nums[1] || nums[1] == nums[2]) return "isosceles";
+        return "scalene";
     }
 };
