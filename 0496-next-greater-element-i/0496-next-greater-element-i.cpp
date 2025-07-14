2510dfa914e2251d0ba1b89135400f61c0308790
ktechbyte <work.khushalsachdeva@gmail.com>
2025-07-14 17:43:43 +0530

    Time: 0 ms (100%), Space: 12.8 MB (60.18%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans(nums1.size(), -1);
-        for(int i = 0; i<nums1.size(); i++){
-            int k = nums1[i];
-            int x = 0;
-            while(k!=nums2[x]) x++;
-            for(int j = x+1; j<nums2.size(); j++){
-                if(nums2[j]>nums2[x]){
-                    ans[i] = nums2[j];
-                    break;
-                }
+        /* O(n^2) brute force approach*/
+        // vector<int> ans(nums1.size(), -1);
+        // for(int i = 0; i<nums1.size(); i++){
+        //     int k = nums1[i];
+        //     int x = 0;
+        //     while(k!=nums2[x]) x++;
+        //     for(int j = x+1; j<nums2.size(); j++){
+        //         if(nums2[j]>nums2[x]){
+        //             ans[i] = nums2[j];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        /*Stack Optimization Approach*/
+        stack<int> st;
+        unordered_map<int, int> ump;
+        vector<int> ans;
+        for(int element : nums2){
+            while(!st.empty() && element > st.top()){
+                ump[st.top()] = element;
+                st.pop();
             }
+            st.push(element);
+        }
+        for(int n : nums1){
+            if(ump.find(n) != ump.end()){
+                ans.push_back(ump[n]);
+            }
+            else ans.push_back(-1);
         }
         return ans;
     }
