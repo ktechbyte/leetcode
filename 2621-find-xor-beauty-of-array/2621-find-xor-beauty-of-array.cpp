f67a4870e7b2d426488f2b321bb8a56f2a744f5a
ktechbyte <work.khushalsachdeva@gmail.com>
2025-07-22 14:55:52 +0530

    Time: 0 ms (100%), Space: 43.6 MB (42.93%) - LeetHub

--- 2621-find-xor-beauty-of-array/2621-find-xor-beauty-of-array.cpp
+++ 2621-find-xor-beauty-of-array/2621-find-xor-beauty-of-array.cpp
@@ -6,5 +6,16 @@ public:
             ans = ans ^ x;
         }
         return ans;
+        // int n = nums.size();
+        // int val = 0, ans = 0;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         for (int k = 0; k < n && k != j; k++) {
+        //             val = ((nums[i] | nums[j]) & nums[k]);
+        //             ans = ans ^ val;
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 };
