7ad9ba6081e01028b5f3b3bcf3c80321d3629b26
ktechbyte <work.khushalsachdeva@gmail.com>
2025-08-03 16:14:08 +0530

    Time: 0 ms (100%), Space: 27.6 MB (14.29%) - LeetHub

--- 3952-trionic-array-i/3952-trionic-array-i.cpp
+++ 3952-trionic-array-i/3952-trionic-array-i.cpp
@@ -5,36 +5,36 @@ public:
         if (n < 3) return false;
         for (int p = 1; p < n - 2; p++) {
             // Check first segment: 0 to p (non-decreasing)
-            bool flag1 = true;
+            bool inc = true;
             for (int i = 0; i < p; i++) {
                 if (nums[i] >= nums[i + 1]) {
-                    flag1 = false;
+                    inc = false;
                     break;
                 }
             }
-            if (!flag1) continue;
+            if (!inc) continue;
 
             for (int q = p + 1; q < n - 1; q++) {
                 // Check second segment: p to q (non-increasing)
-                bool flag2 = true;
+                bool dec = true;
                 for (int i = p; i < q; i++) {
                     if (nums[i] <= nums[i + 1]) {
-                        flag2 = false;
+                        dec = false;
                         break;
                     }
                 }
-                if (!flag2) continue;
+                if (!dec) continue;
 
                 // Check third segment: q to n-1 (non-decreasing)
-                bool flag3 = true;
+                bool inc2 = true;
                 for (int i = q; i < n - 1; i++) {
                     if (nums[i] >= nums[i + 1]) {
-                        flag3 = false;
+                        inc2 = false;
                         break;
                     }
                 }
 
-                if (flag3) return true;
+                if (inc2) return true;
             }
         }
         return false;
