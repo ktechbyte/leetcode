fa69ea6eb2f0280826b3b55e05fe012edcde24a2
ktechbyte <work.khushalsachdeva@gmail.com>
2025-08-27 16:46:11 +0530

    Time: 38 ms (52.91%), Space: 53 MB (15.35%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -7,15 +7,15 @@ public:
         for(int i = 1; i<n+1; i++){
             dp[i][0] = 0;
         }
-        for(int j = 1; j<amount+1; j++){
-            if(j%coins[0] == 0)
-                dp[1][j] = j/coins[0];
-            else
-                dp[1][j] = INT_MAX-1;
-        }
+        // for(int j = 1; j<amount+1; j++){
+        //     if(j%coins[0] == 0)
+        //         dp[1][j] = j/coins[0];
+        //     else
+        //         dp[1][j] = INT_MAX-1;
+        // }
 
         // Process
-        for(int i = 2; i<n+1; i++){
+        for(int i = 1; i<n+1; i++){
             for(int j = 1; j<amount+1; j++){
                 if(coins[i-1] <= j)
                     dp[i][j] = min(dp[i][j-coins[i-1]] + 1, dp[i-1][j]);
