2a34d9978af5da6c8b4f79c1ae8ba7de0cce26f9
ktechbyte <work.khushalsachdeva@gmail.com>
2025-09-04 01:33:57 +0530

    Time: 0 ms (100%), Space: 29.5 MB (83.25%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,11 +1,39 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        // outplace solution
-        vector<int> nums2 = nums;
-        int n = nums.size();
-        for(int i = 0; i<n;i++){
-            nums[(i+k) % n] = nums2[i];
+    void reverseArr(vector<int>& nums, int l, int r) {
+        while (l < r) {
+            swap(nums[l], nums[r]);
+            l++;
+            r--;
         }
+    }
+
+    void rotate(vector<int>& nums, int k) {
+        /* most optimal inplace O(n) The standard way is using reversal algorithm*/
+        int n = nums.size();
+        k %= n; // handle k > n   
+        reverseArr(nums, 0, n - 1); //Reverse the whole array
+        reverseArr(nums, 0, k - 1); //Reverse the first k elements
+        reverseArr(nums, k, n - 1); //Reverse the remaining n - k elements
+
+        /* inplace solution using swappings O(n*k) gives TLE for large k */
+        // int n = nums.size();
+        // int mod = k % n;
+        // int idx = 0;
+        // for(int i = n - mod; i<n; i++){
+        //     int curr = nums[i];
+        //     for(int j = i - 1; j>=idx; j--){
+        //         nums[j+1] = nums[j];
+        //     }
+        //     nums[idx] = curr;
+        //     idx++;
+        // }
+
+        /* outplace solution O(n)*/
+        // vector<int> nums2 = nums;
+        // int n = nums.size();
+        // for(int i = 0; i<n;i++){
+        //     nums[(i+k) % n] = nums2[i];
+        // }
     }
 };
