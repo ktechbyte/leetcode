d1073604fe751eec3cf3c78a0b2d5a437fe51af1
ktechbyte <work.khushalsachdeva@gmail.com>
2025-09-13 00:21:31 +0530

    Time: 3 ms (87.13%), Space: 19.1 MB (25.75%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
         for(char x : s){
-            if(vowels.find(x) != vowels.end()){
+            if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'){
                 return true;
             }
         }
