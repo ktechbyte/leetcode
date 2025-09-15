747b681f5623bb41883db9a13cf83a0ee0a97dd3
ktechbyte <work.khushalsachdeva@gmail.com>
2025-09-15 20:30:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (95.02%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -1,28 +1,53 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
-        set<char> st(brokenLetters.begin(), brokenLetters.end());
-        int i = 0;
+        vector<bool> broken(26, false);
+        for (char c : brokenLetters) {
+            broken[c - 'a'] = true;
+        }
+
         int count = 0;
-        while (i < text.size()) {
-            int j = i;
-            string s = "";
-            bool flag = true;
-            while(j<text.size() && text[j] != ' '){
-                s += text[j];
-                j++;
-            }
-            for(int k = 0; k<s.size(); k++){
-                if(st.find(s[k]) != st.end()){
-                    flag = false;
-                    break;
+        bool canType = true;
+
+        for (int i = 0; i < text.size(); i++) {
+            if (text[i] == ' ') {
+                if (canType) count++;
+                canType = true; // reset for next word
+            } else {
+                if (broken[text[i] - 'a']) {
+                    canType = false;
                 }
             }
-            if(flag){
-                count++;
-            }
-            i = j + 1;
         }
+
+        // check last word
+        if (canType) count++;
+
         return count;
+
+
+        // set<char> st(brokenLetters.begin(), brokenLetters.end());
+        // int i = 0;
+        // int count = 0;
+        // while (i < text.size()) {
+        //     int j = i;
+        //     string s = "";
+        //     bool flag = true;
+        //     while(j<text.size() && text[j] != ' '){
+        //         s += text[j];
+        //         j++;
+        //     }
+        //     for(int k = 0; k<s.size(); k++){
+        //         if(st.find(s[k]) != st.end()){
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
+        //     if(flag){
+        //         count++;
+        //     }
+        //     i = j + 1;
+        // }
+        // return count;
     }
 };
