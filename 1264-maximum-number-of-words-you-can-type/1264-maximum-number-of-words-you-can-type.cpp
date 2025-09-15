7d415c767320936e45a77c8277c183ab2a718bca
ktechbyte <work.khushalsachdeva@gmail.com>
2025-09-15 20:26:52 +0530

    Time: 1 ms (33.89%), Space: 9 MB (57.81%) - LeetHub

---
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        set<char> st;
+        for (char x : brokenLetters) {
+            st.insert(x);
+        }
+        int i = 0;
+        int count = 0;
+        while (i < text.size()) {
+            int j = i;
+            string s = "";
+            bool flag = true;
+            while(j<text.size() && text[j] != ' '){
+                s += text[j];
+                j++;
+            }
+            for(int k = 0; k<s.size(); k++){
+                if(st.find(s[k]) != st.end()){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                count++;
+            }
+            i = j + 1;
+        }
+        return count;
+    }
+};
