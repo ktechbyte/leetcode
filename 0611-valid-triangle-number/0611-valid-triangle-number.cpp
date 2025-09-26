ce5a0e18ba12afda07564423ccd0fd26f8954a58
ktechbyte <work.khushalsachdeva@gmail.com>
2025-09-27 01:09:55 +0530

    Time: 31 ms (69.52%), Space: 16.6 MB (74.73%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -3,6 +3,7 @@ public:
     int triangleNumber(vector<int>& nums) {
         int n = nums.size();
         if(n<3) return 0;
+        // 3sum concept
         sort(nums.begin(), nums.end());
         int count = 0;
         for(int i = n-1; i>1; i--){
