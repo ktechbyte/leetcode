6d0183afdc58f9f2c70b212d67cb38e97d1b2761
ktechbyte <work.khushalsachdeva@gmail.com>
2026-02-09 18:06:39 +0530

    Time: 0 ms (100%), Space: 28 MB (86.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,26 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
+        // sort(nums.begin(), nums.end());
+        // return nums[nums.size()/2];
+
+        // moore's algo
+        int cnt = 0, el;
+        for(int i = 0; i<nums.size(); i++){
+            if(cnt==0){
+                cnt = 1;
+                el = nums[i];
+            } else if(nums[i] == el){
+                cnt++;
+            } else{
+                cnt--;
+            }
+        }
+
+        cnt = 0;
+        for(int x : nums){
+            if(x==el) cnt++;
+        }
+        return cnt>nums.size()/2 ? el : -1;
     }
 };
